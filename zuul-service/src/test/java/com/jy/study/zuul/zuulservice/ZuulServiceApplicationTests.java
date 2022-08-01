a748822d35a6a67452cb780835bc05b34e0e7f04
sazzeo <imjee02@gmail.com>
2022-08-01 16:57:58 +0900

    zuul service 추가

---
+++ zuul-service/src/test/java/com/jy/study/zuul/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.study.zuul.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
